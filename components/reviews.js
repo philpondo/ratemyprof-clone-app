b376e02ab3761d922cc51e6fe8e84dd133b53094
erikalvar <erikalvarbass@gmail.com>
2020-09-01 10:28:40 -0400

    adds reviews index and show

--- components/reviews.js
+++ components/reviews.js
@@ -1,9 +1,38 @@
+const { response } = require('express');
+
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'me',
   host: 'localhost',
-  database: 'rateMyProfessor',
+  database: 'ratemyprofessor',
   password: 'password',
   port: 5432,
 });
 
+const getReviews = (request, response) => {
+  pool.query('SELECT * FROM reviews ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getReviewById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM reviews WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getReviews,
+  getReviewById,
+  // createReview,
+  // updateReview,
+  // deleteReview,
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dbProfessors = require('./components/professors');
+const dbReviews = require('./components/reviews');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({
+    info: "Node.js, Express, and Postgres Rate My Professor clone.",
+  });
+});
+
+// app.get('/professors', dbProfessors.getProfessors);
+// app.get('/professors/:id', dbProfessors.getProfessorById);
+// app.post('/professors', dbProfessors.createProfessor);
+// app.put('/professors/:id', dbProfessors.updateProfessor);
+// app.delete('/professors/:id', dbProfessors.deleteProfessor);
+
+app.get('/reviews', dbReviews.getReviews);
+app.get('/reviews/:id', dbReviews.getReviewsById);
+// app.post('/reviews', dbReviews.createReviews);
+// app.put('/reviews/:id', dbReviews.updateReviews);
+// app.delete('/reviews/:id', dbReviews.deleteReviews);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
