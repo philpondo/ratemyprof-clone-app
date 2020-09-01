c6f0ae9a253574b130e5f8a58ef8a211cd60e400
phil <phil.pondo@gmail.com>
2020-09-01 10:27:01 -0400

    adds professors show & index routes, installs nodemon

--- components/professors.js
+++ components/professors.js
@@ -2,7 +2,36 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "me",
   host: "localhost",
-  database: "rateMyProfessor",
+  database: "ratemyprofessor",
   password: "password",
   port: 5432,
 });
+
+const getProfessors = (request, response) => {
+  pool.query("SELECT * FROM professors ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getProfessorById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query(
+    "SELECT * FROM professors WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
+
+module.exports = {
+  getProfessors,
+  getProfessorById,
+};
